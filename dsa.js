ad88a780c1878d27f18fa92adc98683550cb4d0e
PAnkita123 <malviyaankita901@gmail.com>
2023-03-17 12:51:43 +0530

    push

---
+++ dsa.js
@@ -0,0 +1,52 @@
+// var array = [34, 45, 65, 23, 5, 676, 32, 34]
+// var target = 66
+
+// var sum = 0;
+// var flag = false;
+// for (var i = 0; i <= array.length; i++) {
+//     for (j = 0; j <= array.length; j++)
+//         if (array[i] + array[j] === sum) {
+//             flag = true;
+//         }
+
+// } if (flag === true) {
+//     console.log(array[i], array[j])
+// } else { console.log("not found") }
+
+
+
+// 2nd
+
+
+
+// // 3rd
+
+// for(var i=5768;i<78656;i++){
+
+// }
+
+var number = 19;
+var array = [3, 5, 79, 45, 0, -67, 23, 77, 19, 78, 12]
+
+    for (var i = 0; i < array.length; i++) {
+        if (array.indexOf(number) ===! -1) {
+            return "found"
+        }else{
+            return "not found"
+        }
+    }console.log(array[i])
+
+
+
+// 4th
+var array=[3,5,45,567,78,89,34,67,87,34,67,87,34,78,89]
+
+var keyvaluePair={}
+
+for(var i=0; i<array.length; i++){
+    console.log(array[i])
+}
+
+
+
+
